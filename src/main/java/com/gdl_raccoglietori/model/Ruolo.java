971567b8b85d48675d9dd74db0bd68e0d9d78790
Fabuloso95 <fabioleonardi48@gmail.com>
2025-10-21 15:50:21 +0200

    finito il model di utente e ruolo

---
+++ src/main/java/com/gdl_raccoglietori/model/Ruolo.java
@@ -0,0 +1,7 @@
+package com.gdl_raccoglietori.model;
+
+public enum Ruolo 
+{
+	USER,
+	ADMIN
+}
--- src/main/java/com/gdl_raccoglietori/model/Utente.java
+++ src/main/java/com/gdl_raccoglietori/model/Utente.java
@@ -1,7 +1,13 @@
 package com.gdl_raccoglietori.model;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Entity
@@ -10,20 +16,59 @@ import lombok.*;
 @AllArgsConstructor
 @ToString
 @EqualsAndHashCode
-public class Utente 
+public class Utente implements UserDetails
 {
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(length = 100)
+	@NotBlank(message = "l'username è obbligatorio")
+	@Column(unique = true, nullable = false)
+	@Min(value = 8, message = "l'username deve avere almeno 8 caratteri")
+	private String username;
+	
+	@Column(length = 100, nullable = false)
 	@NotBlank(message = "il nome è obbligatorio")
 	private String nome;
 	
-	@Column(length = 100)
+	@Column(length = 100, nullable = false)
 	@NotBlank(message = "il cognome è obbligatorio")
 	private String cognome;
 	
-	@Column(length = 60)
+	@Column(length = 60, nullable = false)
 	private String password;
+	
+	@Email(message = "Formato email non valido")
+	@NotBlank(message = "l'email è obbligatoria")
+	@Column(nullable = false, unique = true)
+	private String email;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Ruolo ruolo;
+	
+	@Column(nullable = false)
+    private LocalDate dataRegistrazione = LocalDate.now();
+	
+	@Column(length = 255)
+    private String resetPasswordToken;
+
+    private LocalDateTime resetPasswordTokenExpiry;
+
+    @Column(length = 500)
+    private String refreshToken;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() 
+	{
+		return List.of(new SimpleGrantedAuthority("ROLE_" + ruolo.name().toUpperCase()));
+	}
+
+	@Override
+	public String getUsername()
+	{
+		return username;
+	}
 }
