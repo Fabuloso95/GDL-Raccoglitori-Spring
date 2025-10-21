356740892ad4631cdd0f151653c3d1cb2d1c967b
Fabuloso95 <fabioleonardi48@gmail.com>
2025-10-21 15:18:43 +0200

    commit iniziale con piccole modifiche sul model utente

---
+++ src/main/java/com/gdl_raccoglietori/model/Utente.java
@@ -0,0 +1,22 @@
+package com.gdl_raccoglietori.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Utente 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 100)
+	private String nome;
+}
