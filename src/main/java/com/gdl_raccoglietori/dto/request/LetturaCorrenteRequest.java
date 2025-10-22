eaba387725a06ad03fbd4804f9f88155c5337754
Fabuloso95 <fabioleonardi48@gmail.com>
2025-10-22 15:10:16 +0200

    creati i dto request necessari

--- src/main/java/com/gdl_raccoglietori/dto/request/LetturaCorrenteRequest.java
+++ src/main/java/com/gdl_raccoglietori/dto/request/LetturaCorrenteRequest.java
@@ -1,5 +1,15 @@
 package com.gdl_raccoglietori.dto.request;
 
-public class LetturaCorrenteRequest {
+import jakarta.validation.constraints.*;
 
+public class LetturaCorrenteRequest 
+{
+	@NotNull(message = "L'ID del libro è obbligatorio per iniziare una lettura")
+    @Positive(message = "L'ID del libro deve essere un valore positivo")
+    private Long libroId;
+    
+    // Se l'utente vuole impostare la pagina iniziale a zero o a un valore specifico.
+    // Lo rendiamo facoltativo; se non specificato, il controller imposterà 0.
+    @Positive(message = "La pagina iniziale deve essere un valore positivo")
+    private Integer paginaIniziale;
 }
